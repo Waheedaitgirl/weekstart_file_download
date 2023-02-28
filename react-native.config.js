d0c92d43f7e43d927536a4e0d61d89e15ade8c7c
waheeda <waheeda@recruitbpm.com>
2023-02-28 16:18:23 +0500

    check the code

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+        android:null,
+      },
+    },
+  },
+  project: {
+    ios: {},
+    android: {}, // grouped into "project"
+  },
+  assets: ['./src/assets/fonts/'], // stays the same
+};
